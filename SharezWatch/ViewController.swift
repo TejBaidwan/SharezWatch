3c262dac88248c9aca272b196e9064d25f3b8023
Tej Baidwan <w0806279@myscc.ca>
2024-09-28 15:49:29 -0400

    Initial Commit

---
+++ SharezWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharezWatch
+//
+//  Created by Tejvir Baidwan on 2024-09-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
