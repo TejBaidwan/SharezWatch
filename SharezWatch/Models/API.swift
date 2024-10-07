3128bcd3d05d01c978f12c8dbea092a9b698a84a
Tej Baidwan <w0806279@myscc.ca>
2024-10-07 16:30:14 -0400

    Added my API token which I will use to conduct API searches with StockData.org

---
+++ SharezWatch/Models/API.swift
@@ -0,0 +1,11 @@
+//
+//  API.swift
+//  SharezWatch
+//
+//  Created by Tejvir Baidwan on 2024-10-07.
+//
+
+import Foundation
+
+//This is my API key
+let API_KEY = "400wr2eu76ck63WkNF0r4ADF6fq9nbiVkzal02GL"
