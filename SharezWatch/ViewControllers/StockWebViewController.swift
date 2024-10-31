74b38ec585d7df8b0427d40d054ea6909fc70f46
Tej Baidwan <w0806279@myscc.ca>
2024-10-30 21:56:47 -0400

    Created the stock web VC which will show a full-screen webview with charting info about a watchlisted stock, and this is shown by double tapping a watchlisted stock

---
+++ SharezWatch/ViewControllers/StockWebViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StockWebViewController.swift
+//  SharezWatch
+//
+//  Created by Tejvir Baidwan on 2024-10-30.
+//
+
+import UIKit
+import WebKit
+
+/**
+ This class represents the Stock webview which is shown when a watchlisted stock is double tapped
+ */
+class StockWebViewController: UIViewController {
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var stockWeb: WKWebView!
+    
+    
+    
+}
