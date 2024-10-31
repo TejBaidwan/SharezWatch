becb4347ae8007a5bed448ab6c245705f78e8323
Tej Baidwan <w0806279@myscc.ca>
2024-10-30 21:52:06 -0400

    Added the ability to long press on a collectionview cell in the watchlist VC and delete a stock from the watchlist

--- SharezWatch/ViewControllers/WatchlistedStocksViewController.swift
+++ SharezWatch/ViewControllers/WatchlistedStocksViewController.swift
@@ -33,15 +33,19 @@ class WatchlistedStocksViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        createSnapshot()
         watchlistCollection.delegate = self
+        createSnapshot()
+        
+        //Create a LongPress gesture recognizer and apply it to the collection view
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+        watchlistCollection.addGestureRecognizer(longPressGesture)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        createSnapshot()
         watchlistCollection.delegate = self
+        createSnapshot()
     }
     
     //MARK: - Loading of Data using DiffableDataSource into the CollectionView
@@ -49,9 +53,18 @@ class WatchlistedStocksViewController: UIViewController {
     lazy var collectionViewDataSource = UICollectionViewDiffableDataSource<Section, Stock>(collectionView: watchlistCollection) { collectionView, indexPath, watchlistedStock in
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "watchlistedStock", for: indexPath) as! WatchlistCollectionViewCell
         
+        if let customFontContent = UIFont(name: "KulimPark-Bold", size: 17) {
+            cell.watchlistPrice.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: customFontContent)
+            cell.watchlistChange.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: customFontContent)
+            cell.watchlistQuantity.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: customFontContent)
+        }
+        if let customFontTitle = UIFont(name: "KulimPark-Bold", size: 32) {
+            cell.watchlistTicker.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: customFontTitle)
+        }
+        
         cell.watchlistTicker.text = watchlistedStock.ticker
         cell.watchlistPrice.text = "Price: " + (self.priceFormatter.string(from: NSNumber(value: watchlistedStock.price)) ?? "0.00")
-        cell.watchlistChange.text = "Change: " + String(watchlistedStock.dayChange)
+        cell.watchlistChange.text = "Change %: " + String(watchlistedStock.dayChange)
         cell.watchlistQuantity.text = "Quantity: " + "0"
         
         if(watchlistedStock.dayChange < 0) {
@@ -71,6 +84,29 @@ class WatchlistedStocksViewController: UIViewController {
         collectionViewDataSource.apply(snapshot)
     }
     
+    // MARK: - Long Press Gesture Handler for deleting a Stock
+    @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            
+            //Get the Stock object which is long pressed
+            let location = gestureRecognizer.location(in: watchlistCollection)
+            if let indexPath = watchlistCollection.indexPathForItem(at: location) {
+                let stockToRemove = stockStore.allStocks[indexPath.item]
+                
+                //Create an Alert Controller with options to accept or decline deletion
+                let alertController = UIAlertController(title: "Delete Stock?", message: "Confirm deletion of \(stockToRemove.ticker)?", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                alertController.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { _ in
+                    
+                    //If the deletion is confirmed then remove the Stock from the saved directory and refresh the page
+                    self.stockStore.removeStock(stock: stockToRemove)
+                    self.createSnapshot()
+                }))
+                present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
 }
 
 //MARK: - Extension Methods for setting the CollectionView sizes
@@ -79,12 +115,12 @@ extension WatchlistedStocksViewController: UICollectionViewDelegateFlowLayout{
     
     //Line spacing in the collection view
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        return 5
     }
     
     //Interim spacing in the collection view
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
+        return 10
     }
     
     //Determining size for collection view cells based on landscape or portrait orientation
@@ -101,11 +137,10 @@ extension WatchlistedStocksViewController: UICollectionViewDelegateFlowLayout{
         }
         
         //Calculate item width and height
-        let spacing = (numberOfItems - 1) * 1
+        let spacing = (numberOfItems - 1) * 10
         let itemWidth = (collectionViewWidth - spacing) / numberOfItems
-        let itemHeight = itemWidth
 
-        return CGSize(width: itemWidth, height: itemHeight)
+        return CGSize(width: itemWidth, height: itemWidth)
         
     }
 }
